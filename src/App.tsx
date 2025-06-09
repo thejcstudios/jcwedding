92816004bc70a9a1a54311f48bd6b5b175cfd5c2
MacBook Air <Jcstudios.database@gmail.com>
2025-06-09 22:51:23 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import './index.css'
 import Layout from './components/Layout';
-import Navbar from './Navbar';
+import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Invitation from './components/Invitation';
 import About from './components/About';
