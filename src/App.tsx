ebd1e96c8ea4188af9771351027f6a79935fcce0
MacBook Air <Jcstudios.database@gmail.com>
2025-06-09 23:18:17 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import './index.css'
 import Layout from './components/Layout';
-import Navbar from './components/Navbar';
+import Navbar1 from './components/Navbar1';
 import Hero from './components/Hero';
 import Invitation from './components/Invitation';
 import About from './components/About';
@@ -12,7 +12,7 @@ function App() {
   return (
    <>
    <Layout>
-   <Navbar/>
+   <Navbar1/>
    <Hero/>
    <Invitation/>
    <About/>
